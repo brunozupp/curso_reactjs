254f289401a88d7dfe0511d3fa54a151cfd4f21d
Bruno Noveli <brunimzuppwolf@gmail.com>
2021-10-05 20:33:14 -0300

    PropTypes

---
+++ src/aulas/ModuloMaisReact/AulaPrototype1/AulaPrototype1.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Button from './Button.js'
+const AulaPrototype1 = () => {
+    return (
+        <div>
+            <Button width={300} margin={"50px"}>Clique Aqui</Button>
+        </div>
+    )
+}
+
+export default AulaPrototype1
